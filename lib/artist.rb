f89b5d2b516b37fa2a94be6a8c40e70a231c474f
Ryan Brady <ryanbrady369@gmail.com>
2018-11-09 22:54:35 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -21,7 +21,7 @@ class Artist
   end
 
   def genres
-    self.songs.map {|song| song.genre} 
+    self.songs.map {|song| song.genre}
     #   if song.artist == self
     #     song.genre
     #   end
